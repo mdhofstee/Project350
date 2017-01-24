b114b9618b579e95c76f1f25767a49f9096ed543
AndrewOverman <AndrewOverman0517@gmail.com>
2017-01-24 13:36:41 -0500

    Add files via upload

--- Dice.java
+++ Dice.java
@@ -1,5 +1,19 @@
 package project350;
 
+import java.util.Random;
+
 public class Dice {
 
+	int Value = 0;
+	Random r = new Random();
+	final int Sides = 6;
+
+	public void Random() {
+		Value = (r.nextInt(Sides) + 1);
+	}
+
+	public int Roll() {
+		Random();
+		return Value;
+	}
 }
