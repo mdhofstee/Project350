19a518d6871e80d377e77616ec825fe8ec8ca381
AndrewOverman <AndrewOverman0517@gmail.com>
2017-01-24 14:20:27 -0500

    Add files via upload

---
+++ YahtzeeGUI.java
@@ -0,0 +1,15 @@
+package project350;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class YahtzeeGUI {
+	public void load(){
+		JFrame guiFrame = new JFrame();
+		guiFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		guiFrame.setTitle("Yahtzee!");
+		guiFrame.setSize(1980,1080);
+		guiFrame.setVisible(true);
+	}
+}
+
