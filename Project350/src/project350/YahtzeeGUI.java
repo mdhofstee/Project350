9400e7f71ca7087406f1eadfce7a4d6ea693803c
AndrewOverman <AndrewOverman0517@gmail.com>
2017-02-15 18:12:58 -0500

    Update YahtzeeGUI.java

--- Project350/src/project350/YahtzeeGUI.java
+++ Project350/src/project350/YahtzeeGUI.java
@@ -1,5 +1,14 @@
 package project350;
 
-public class YahtzeeGUI {
+import javax.swing.*;
+import java.awt.*;
 
+public class YahtzeeGUI {
+	public void load(){
+		JFrame guiFrame = new JFrame();
+		guiFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		guiFrame.setTitle("Yahtzee!");
+		guiFrame.setSize(1980,1080);
+		guiFrame.setVisible(true);
+	}
 }
