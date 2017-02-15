0aae343226d7ec624c2338ce1de6db7a1fa7482f
AndrewOverman <AndrewOverman0517@gmail.com>
2017-02-15 18:09:55 -0500

    Update Dice.java

--- Project350/src/project350/Dice.java
+++ Project350/src/project350/Dice.java
@@ -1,5 +1,19 @@
 package project350;
 
+import java.util.Random;
+
 public class Dice {
 
+	private int value = 0;
+	Random r = new Random();
+	final int SIDES = 6;
+
+	public void Random() {
+		value = (r.nextInt(Sides) + 1);
+	}
+
+	public int Roll() {
+		Random();
+		return value;
+	}
 }
