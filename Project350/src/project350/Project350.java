1598d852a92dbb2bafb94ffe5ce784be659ff705
AndrewOverman <AndrewOverman0517@gmail.com>
2017-02-15 18:11:46 -0500

    Update Project350.java

--- Project350/src/project350/Project350.java
+++ Project350/src/project350/Project350.java
@@ -1,5 +1,10 @@
 package project350;
 
 public class Project350 {
-
+	
+	public static void main(String[] args) {
+		Yahtzee game = new Yahtzee();
+		YahtzeeGUI board = new YahtzeeGUI();
+		Yahtzee.run(game, board);		
+	}	
 }
