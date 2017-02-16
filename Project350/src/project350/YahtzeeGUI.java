1a42f6b59273f258f92d9694ff1ed67cab5fb918
AndrewOverman <AndrewOverman0517@gmail.com>
2017-02-15 19:53:46 -0500

    JTextArea

--- Project350/src/project350/YahtzeeGUI.java
+++ Project350/src/project350/YahtzeeGUI.java
@@ -4,11 +4,29 @@ import javax.swing.*;
 import java.awt.*;
 
 public class YahtzeeGUI {
+	
+	JFrame guiFrame = new JFrame();
+	JTextArea ones, twos, threes;
 	public void load(){
-		JFrame guiFrame = new JFrame();
+		
 		guiFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		guiFrame.setTitle("Yahtzee!");
 		guiFrame.setSize(1980,1080);
 		guiFrame.setVisible(true);
+		ScoreBoxes();
+	}
+	
+	public void ScoreBoxes(){
+		ones = new JTextArea();
+		ones.setSize(100,20);
+		guiFrame.add(ones);
+		
+		twos = new JTextArea();
+		twos.setSize(100,20);
+		guiFrame.add(twos);
+		
+		threes = new JTextArea();
+		threes.setSize(100,20);
+		guiFrame.add(threes);
 	}
 }
