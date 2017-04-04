279bfb8498927c40942d3922c9008d6f7f5d6bff
AndrewOverman <AndrewOverman0517@gmail.com>
2017-04-03 22:03:10 -0400

    Update YahtzeeGUI.java

--- Project350/src/project350/YahtzeeGUI.java
+++ Project350/src/project350/YahtzeeGUI.java
@@ -1,17 +1,24 @@
-package project.cis350;
+package project350;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.border.Border;
+import javax.swing.event.MenuEvent;
+import javax.swing.event.MenuListener;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 /**
  * 
  * @author Overman, Hofstee
@@ -29,7 +36,7 @@ public class YahtzeeGUI implements ActionListener {
 	/**
 	 * 
 	 */
-	private int size =  Integer.parseInt(JOptionPane.showInputDialog("Number of Players?"));
+	private int size = Integer.parseInt(JOptionPane.showInputDialog("Number of Players?"));
 	/**
 	 * 
 	 */
@@ -38,6 +45,10 @@ public class YahtzeeGUI implements ActionListener {
 	 * 
 	 */
 	private Border border = BorderFactory.createLineBorder(Color.BLACK);
+	/**
+	 * 
+	 */
+	private Font myFont = new Font("Serif", Font.BOLD, 16);
 	/**
 	 * 
 	 */
@@ -50,6 +61,18 @@ public class YahtzeeGUI implements ActionListener {
 	 * 
 	 */
 	private Player[] players = new Player[size];
+	/**
+	 * 
+	 */
+	private Player[] fakeplayers = new Player[size];
+	/**
+	 * 
+	 */
+	private JMenuBar menuBar;
+	/**
+	 * 
+	 */
+	private JMenu save, load;
 	/**
 	 * 
 	 */
@@ -70,7 +93,10 @@ public class YahtzeeGUI implements ActionListener {
 	 * 
 	 */
 	private final JCheckBox checkBox5 = new JCheckBox();
-	
+	/**
+	 * 
+	 */
+	private Boolean nextTurn = false;
 	/**
 	 * 
 	 */
@@ -83,8 +109,20 @@ public class YahtzeeGUI implements ActionListener {
 		
 		for (int i = 0; i < size; i++) {
 			players[i] = new Player();
-			System.out.println(players[i]);
+			fakeplayers[i] = new Player();
 		}
+		
+		menuBar = new JMenuBar();
+		save = new JMenu("Save");
+		save.addMenuListener(new SampleMenuListener());
+		save.setMnemonic(KeyEvent.VK_S);
+		load = new JMenu("Load");
+		load.addMenuListener(new SampleMenuListener());
+		load.setMnemonic(KeyEvent.VK_L);
+		menuBar.add(save);
+		menuBar.add(load);
+		guiFrame.setJMenuBar(menuBar);
+		
 		scoreBoxes();
 		labels();
 	}
@@ -96,7 +134,8 @@ public class YahtzeeGUI implements ActionListener {
 		for (int r = 0; r < 21; r++) {
             for (int c = 0; c < size + 1; c++) {         
                 buttons[r][c] = new JButton("0");
-                buttons[r][c].setPreferredSize(new Dimension(100, 30));
+                buttons[r][c].setFont(myFont);
+                buttons[r][c].setPreferredSize(new Dimension(200, 40));
                 buttons[r][c].addActionListener(this);
                 guiFrame.add(buttons[r][c]);
             }
@@ -109,78 +148,104 @@ public class YahtzeeGUI implements ActionListener {
 	 */
 	public void labels() {
 		buttons[0][0].setText("Players: "); 
-		buttons[0][0].setEnabled(false);		
+		buttons[0][0].setFont(myFont);
+		buttons[0][0].setEnabled(false);	
+		
 		for (int i = 1; i < size + 1; i++) {
 			buttons[0][i].setText("Player " + i);
+			buttons[0][i].setFont(myFont);
 			buttons[0][i].setEnabled(false);
 		}
+		
 		buttons[1][0].setText("Aces: ");
+		buttons[1][0].setFont(myFont);
 		buttons[1][0].setEnabled(false);
+		
 		buttons[2][0].setText("Twos: ");
+		buttons[2][0].setFont(myFont);
 		buttons[2][0].setEnabled(false);
+		
 		buttons[3][0].setText("Threes: ");
+		buttons[3][0].setFont(myFont);
 		buttons[3][0].setEnabled(false);
+		
 		buttons[4][0].setText("Fours: ");
+		buttons[4][0].setFont(myFont);
 		buttons[4][0].setEnabled(false);
+		
 		buttons[5][0].setText("Fives: ");
-		buttons[5][0].setEnabled(false);	
+		buttons[5][0].setFont(myFont);
+		buttons[5][0].setEnabled(false);
+		
 		buttons[6][0].setText("Sixes: ");
+		buttons[6][0].setFont(myFont);
 		buttons[6][0].setEnabled(false);
+		
 		buttons[7][0].setText("Total Score: ");
+		buttons[7][0].setFont(myFont);
 		buttons[7][0].setEnabled(false);
 		
 		buttons[8][0].setText("Bonus: ");
+		buttons[8][0].setFont(myFont);
 		buttons[8][0].setEnabled(false);
 		
 		buttons[9][0].setText("Total: ");
+		buttons[9][0].setFont(myFont);
 		buttons[9][0].setEnabled(false);
 		
 		buttons[10][0].setText("3 of a Kind: ");
+		buttons[10][0].setFont(myFont);
 		buttons[10][0].setEnabled(false);
 		
 		buttons[11][0].setText("4 of a Kind: ");
+		buttons[11][0].setFont(myFont);
 		buttons[11][0].setEnabled(false);
 		
 		buttons[12][0].setText("Full House: ");
+		buttons[12][0].setFont(myFont);
 		buttons[12][0].setEnabled(false);
 		
 		buttons[13][0].setText("Small Straight: ");
+		buttons[13][0].setFont(myFont);
 		buttons[13][0].setEnabled(false);
 		
 		buttons[14][0].setText("Large Straight: ");
+		buttons[14][0].setFont(myFont);
 		buttons[14][0].setEnabled(false);
 		
 		buttons[15][0].setText("Yahtzee: ");
+		buttons[15][0].setFont(myFont);
 		buttons[15][0].setEnabled(false);
 		
 		buttons[16][0].setText("Chance: ");
+		buttons[16][0].setFont(myFont);
 		buttons[16][0].setEnabled(false);
 		
 		buttons[17][0].setText("Yahtze Bonus: ");
+		buttons[17][0].setFont(myFont);
 		buttons[17][0].setEnabled(false);
 		
 		buttons[18][0].setText("Lower Total: ");
+		buttons[18][0].setFont(myFont);
 		buttons[18][0].setEnabled(false);
 		
 		buttons[19][0].setText("Upper Total: ");
+		buttons[19][0].setFont(myFont);
 		buttons[19][0].setEnabled(false);
 		
 		buttons[20][0].setText("Grand Total: ");
+		buttons[20][0].setFont(myFont);
 		buttons[20][0].setEnabled(false);
 	}
-	
 	/**
 	 * 
 	 */
 	public void turn() {		
 		Dice dice = new Dice();
-		int ones = 0;
-		int twos = 0;
-		int threes = 0;
-		int fours = 0;
-		int fives = 0;
-		int sixes = 0;
 		
+		Object[] nextturn = {"It's now player " + playerturn + "'s turn"};
+		
+		JOptionPane.showConfirmDialog(null, nextturn, "Player " + playerturn + "'s turn", JOptionPane.OK_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE);
 		for (int k = 0; k < 5; k++) {
 			rolls[k] = dice.roll();
 		}
@@ -222,58 +287,95 @@ public class YahtzeeGUI implements ActionListener {
 			for (int i = 0; i < 5; i++) {
 
 				if (rolls[i] == 1) {
-					ones += 1;
+					players[playerturn - 1].setOnes(players[playerturn - 1].getOnes() + 1);
 				}
 				if (rolls[i] == 2) {
-					twos += 1;
+					players[playerturn - 1].setTwos(players[playerturn - 1].getTwos() + 1);
 				}
 				if (rolls[i] == 3) {
-					threes += 1;
+					players[playerturn - 1].setThrees(players[playerturn - 1].getThrees() + 1);
 				}
 				if (rolls[i] == 4) {
-					fours += 1;
+					players[playerturn - 1].setFours(players[playerturn - 1].getFours() + 1);
 				}
 				if (rolls[i] == 5) {
-					fives += 1;
+					players[playerturn - 1].setFives(players[playerturn - 1].getFives() + 1);
 				}
 				if (rolls[i] == 6) {
-					sixes += 1;
+					players[playerturn - 1].setSixes(players[playerturn - 1].getSixes() + 1);
 				}
 			}
-				players[playerturn - 1].setOnes(ones);
+				players[playerturn - 1].getOnes();
 				buttons[1][playerturn].setText(Integer.toString(players[playerturn - 1].getOnes()));
 				
-				players[playerturn - 1].setTwos(twos);
+				players[playerturn - 1].getTwos();
 				buttons[2][playerturn].setText(Integer.toString(players[playerturn - 1].getTwos()));
 				
-				players[playerturn - 1].setThrees(threes);
+				players[playerturn - 1].getThrees();
 				buttons[3][playerturn].setText(Integer.toString(players[playerturn - 1].getThrees()));
 				
-				players[playerturn - 1].setFours(fours);
+				players[playerturn - 1].getFours();
 				buttons[4][playerturn].setText(Integer.toString(players[playerturn - 1].getFours()));
 				
-				players[playerturn - 1].setFives(fives);
+				players[playerturn - 1].getFives();
 				buttons[5][playerturn].setText(Integer.toString(players[playerturn - 1].getFives()));
 				
-				players[playerturn - 1].setSixes(sixes);
+				players[playerturn - 1].getSixes();
 				buttons[6][playerturn].setText(Integer.toString(players[playerturn - 1].getSixes()));
+				
+				
+				
 			
 		if (playerturn == size) {
 			playerturn = 1;
 		} else {
 			playerturn += 1;
+			nextTurn = false;
 		}		
-		Object[] nextturn = {"It's now player " + playerturn + "'s turn"};
-		
-		JOptionPane.showConfirmDialog(null, nextturn , "Player " + playerturn + "'s turn",
-				JOptionPane.OK_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE);	}  
-	
+			
+	}  
 	/**
-	 * @param evt standard event listener 
+	 * @param arg0 standard event listener 
 	 */
-	public void actionPerformed(ActionEvent evt) {
-        	if (buttons[1][playerturn].isSelected()) {
-        		buttons[1][playerturn].setEnabled(false);
-        	} 
+	@Override
+	public void actionPerformed(final ActionEvent arg0) {
+		if (arg0.getSource() ==  buttons[1][1]){
+			nextTurn = true;
+			turn();
 		}
+		
+	}
+	/*
+	 * listens for actions in the Jmenubar.
+	 */
+	class SampleMenuListener implements MenuListener{
+
+		@Override
+		public void menuCanceled(final MenuEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void menuDeselected(final MenuEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void menuSelected(final MenuEvent e) {
+			if (e.getSource() == save) {
+				for (int i = 0; i < players.length; i++) {
+					fakeplayers[i] = players[i];
+				}
+				System.out.println(fakeplayers[0]);
+			}
+			if (e.getSource() == load) {
+				for (int i = 0; i < players.length; i++) {
+					players[i] = fakeplayers[i];
+				}
+			}
+			
+		}
+	}
 }
